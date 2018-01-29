c4a6027b74190e0ddaca2895ed61a606871c03b6
decksterr <arthurpicerna@gmail.com>
2018-01-29 06:52:52 -0500

    gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,71 @@
-var gulp          = require('gulp');
-var rename        = require('gulp-rename');
-var sass          = require('gulp-sass');
-var autoprefixer  = require('gulp-autoprefixer');
-var cleanCSS      = require('gulp-csso');
-var sourcemaps    = require('gulp-sourcemaps');
+const gulp          = require('gulp');
+const path        = require('path');
+const fs          = require('fs');
+const argv = require('yargs').argv;
 
-var argv          = require('yargs').argv;
-var isProduction  = (argv.production === undefined) ? false : true;
+const postcss      = require('gulp-postcss');
+const rename        = require('gulp-rename');
+const sourcemaps    = require('gulp-sourcemaps');
+
+const isDirectory = source => fs.lstatSync(source).isDirectory();
+const getDirectories = source =>
+    fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+
+let aliasMap = getDirectories('./web/assets').reduce(function (acc, cur, i) {
+    const folder = cur.match(/\/(\w*)_assets$/)[1];
+    acc[folder] = `web/assets/${folder}_assets/css`;
+    return acc;
+}, {});
+console.log(aliasMap);
+
+let assets_folder = '';
+if (typeof argv.f !== 'undefined') {
+    assets_folder = argv.f;
+} else if (typeof argv.folder !== 'undefined') {
+    assets_folder = argv.folder;
+}
+console.log(assets_folder);
+process.exit();
+
+const sassChain = function(assets) {
+    return gulp.src(assets)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.resolve('../')));
+};
+
+const postcssChain = function (assets) {
+    return gulp.src(assets)
+        .pipe(cleanCSS({ compatibility: 'ie9' }))
+        .pipe(rename({ basename: 'style', extname: '.min.css' }))
+        .pipe(path.resolve('../'));
+};
 
 gulp.task('watch', function () {
     gulp.watch([
-        'web/assets/*_assets/css/scss/*.scss', 
-        'web/assets/*_assets/css/*.css', 
+        'web/assets/*_assets/css/scss/*.scss',
+        'web/assets/*_assets/css/*.css',
         '!web/assets/*_assets/css/main.css'
     ], ['postcss']);
 });
 
 gulp.task('sass', function () {
-    return gulp.src('web/assets/*_assets/css/scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'));
+    let folders = (assets_folder.length) ? [aliasMap[f]] : Object.values(aliasMap);
+    for (folder of folders) {
+        sassChain(folder);
+    }
+    return 'ok';
 });
 
 gulp.task('postcss', ['sass'], function () {
-    return gulp.src('css/main.css')
-        .pipe(cleanCSS({compatibility: 'ie9'}))
-        .pipe(rename({basename: 'style', extname: '.min.css'}))
-        .pipe(gulp.dest('dist'));
+    let folders = (assets_folder.length) ? [aliasMap[f]] : Object.values(aliasMap);
+    for (folder of folders) {
+        postcssChain(folder);
+    }
+    return 'ok';
 });
 
 gulp.task('default', ['postcss']);
+gulp.task('main', ['postcss']);
--- tasks/test.js
+++ tasks/test.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const fs = require('fs');
 
+var argv = require('yargs').argv;
+var isProduction = (argv.production === undefined) ? false : true;
+console.log(argv);
+
 const isDirectory = source => fs.lstatSync(source).isDirectory();
 const getDirectories = source =>
     fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
 
-let alias = getDirectories('../src/Aviatur').reduce(function (acc, cur, i) {
-    let bundle = cur.match(/\/(\w*Bundle)$/)[1];
-    acc[bundle] = bundle + '/Resources/public/js';
+let aliasMap = getDirectories('../web/assets').reduce(function (acc, cur, i) {
+    const folder = cur.match(/\/(\w*_assets)$/)[1];
+    acc[folder] = folder + '/css';
     return acc;
 }, {});
-console.log(alias);
+console.log(aliasMap);
