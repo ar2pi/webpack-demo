477defa4d209eb3d21a48d197537912b1b576e5f
ARTHUR DOMINIQUE BERNARD PICERNA <arthur.picerna@aviatur.com.co>
2018-01-31 21:17:24 -0500

    gulp continued

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,167 @@
 const gulp          = require('gulp');
-const path        = require('path');
-const fs          = require('fs');
-const argv = require('yargs').argv;
+const fs            = require('fs');
+const path          = require('path');
+const argv          = require('yargs').argv;
+const moment        = require('moment');
+const chalk         = require('chalk');
+const chokidar      = require('chokidar');
 
-const postcss      = require('gulp-postcss');
+const concat        = require('gulp-concat');
 const rename        = require('gulp-rename');
+const postcss       = require('gulp-postcss');
 const sourcemaps    = require('gulp-sourcemaps');
 
+const mdcss         = require('mdcss');
+const autoprefixer  = require('autoprefixer');
+const uncomment     = require('postcss-discard-comments');
+const cachebuster   = require('postcss-cachebuster');
+const sass          = require('postcss-node-sass');
+const csso          = require('postcss-csso');
+
 const isDirectory = source => fs.lstatSync(source).isDirectory();
+const isFile = source => fs.lstatSync(source).isFile();
 const getDirectories = source =>
     fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+const getFiles = source => 
+    fs.readdirSync(source).map(name => path.join(source, name)).filter(isFile);
 
-let aliasMap = getDirectories('./web/assets').reduce(function (acc, cur, i) {
-    const folder = cur.match(/\/(\w*)_assets$/)[1];
-    acc[folder] = `web/assets/${folder}_assets/css`;
+let aliasMap = getDirectories(path.join('.', 'web', 'assets')).reduce((acc, cur, i) => {
+    let folder = cur.match(/[\/\\](\w*)_assets$/);
+    if (folder !== null) {
+        acc[folder[1]] =  path.join('web', 'assets', `${folder[1]}_assets`, 'css');
+    }
     return acc;
 }, {});
-console.log(aliasMap);
+let aliasEntries = entry => (entry.length) ? [aliasMap[entry]] : Object.values(aliasMap);
 
-let assets_folder = '';
-if (typeof argv.f !== 'undefined') {
-    assets_folder = argv.f;
-} else if (typeof argv.folder !== 'undefined') {
-    assets_folder = argv.folder;
-}
-console.log(assets_folder);
-process.exit();
+let assets_folder = (argv.d) ? argv.d : (argv.folder) ? argv.folder : '';
+let assets_folder_paths = aliasEntries(assets_folder);
 
-const sassChain = function(assets) {
-    return gulp.src(assets)
+const sassChain = src => {
+    let dest = path.resolve(path.dirname(src), '../');
+    let plugins = [
+        sass(),
+        autoprefixer()
+    ];
+    return gulp.src(src)
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
+        .pipe(postcss(plugins))
+        .pipe(rename({
+            dirname: dest,
+            basename: 'main', 
+            extname: '.css'
+        }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.resolve('../')));
+        .pipe(gulp.dest(dest));
 };
 
-const postcssChain = function (assets) {
-    return gulp.src(assets)
-        .pipe(cleanCSS({ compatibility: 'ie9' }))
-        .pipe(rename({ basename: 'style', extname: '.min.css' }))
-        .pipe(path.resolve('../'));
+const postSassChain = src => {
+    const cssFiles = getFiles(src).filter(file => /\.css$/.test(file));
+    let mainCss = cssFiles.filter(file => /^main$/.test(path.basename(file, '.css')));
+    let customCss = cssFiles.filter(file => !/^(?:main)|(?:main.min)$/.test(path.basename(file, '.css')));
+    let dest = path.resolve(src);
+    let plugins = [
+        uncomment({
+            removeAll: true
+        }),
+        cachebuster({
+            imagesPath: '/web',
+            cssPath: `/${src.replace(/\\/g, '/')}`
+        }),
+        csso()
+    ];
+    return gulp.src([...mainCss, ...customCss])
+        .pipe(concat('main.min.css'))
+        .pipe(postcss(plugins))
+        .pipe(rename({
+            dirname: dest
+        }))
+        .pipe(gulp.dest(dest));
 };
 
-gulp.task('watch', function () {
-    gulp.watch([
+const docChain = src => {
+    let mainCss = getFiles(src).filter(file => /main.css$/.test(file));
+    let dest = path.resolve(src);
+    let plugins = [
+        mdcss({
+            destination: `/${src.replace(/\\/g, '/')}/styleguide`,
+            index: 'index.html'
+        })
+    ];
+    return gulp.src(mainCss)
+        .pipe(postcss(plugins))
+        .pipe(rename({
+            dirname: dest
+        }))
+        .pipe(gulp.dest(dest));
+};
+
+const taskRunner = (tasks, done) => {
+    for (folder of assets_folder_paths) {
+        let assetsFolder = /[\/\\].*_assets[\/\\]/.test(folder) ? folder.replace(/.*[\/\\](.*_assets)[\/\\].*/, '$1') : folder;
+        let tasksGroup = tasks.map(t => chalk.bold.rgb(0,128,128)(t));
+        console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] Starting [${tasksGroup.join(', ')}'] in ${assetsFolder}`);
+        for (let i = 0; i < tasks.length; i++) {
+            switch(tasks[i]) {
+                case 'sass':
+                    console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] - Writing ${chalk.bold('main.css')}`);
+                    sassChain(path.join(folder, 'scss', 'main.scss'));
+                    break;
+                case 'postSass':
+                    console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] - Writing ${chalk.bold('main.min.css')}`);
+                    postSassChain(folder);
+                    break;
+                case 'doc':
+                    console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] - Generating doc in ./styleguide`);
+                    docChain(folder);
+                    break;
+            }
+            console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] - Done with '${tasksGroup[i]}'`);
+        }
+        console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] Finished [${tasksGroup.join(', ')}] in ${assetsFolder}`);
+    }
+    return done();
+};
+
+gulp.task('watch', () => {
+    let watched = [
         'web/assets/*_assets/css/scss/*.scss',
         'web/assets/*_assets/css/*.css',
-        '!web/assets/*_assets/css/main.css'
-    ], ['postcss']);
+    ];
+    let cwd = new RegExp(`^web[\\/\\\\]assets[\\/\\\\]${assets_folder || '.*'}_assets[\\/\\\\]`);
+    let ignored = [
+        new RegExp(`[\\/\\\\]css[\\/\\\\]main(?:\\.min)?\\.css$`)
+    ];
+    chokidar.watch(watched).on('change', (fpath, stats) => {
+        if (cwd.test(fpath)) {
+            for (let ignore of ignored) {
+                if (ignore.test(fpath))
+                    return;
+            }
+            console.log(`[${chalk.gray(moment().format('HH[:]mm[:]ss'))}] ${chalk.bold(fpath)} changed`);
+            gulp.parallel('css')();
+        }
+    }).on('error', error => {
+        console.log(`Watcher error: ${error}`);
+    });
 });
 
-gulp.task('sass', function () {
-    let folders = (assets_folder.length) ? [aliasMap[f]] : Object.values(aliasMap);
-    for (folder of folders) {
-        sassChain(folder);
-    }
-    return 'ok';
+
+gulp.task('css', (done) => {
+    return taskRunner(['sass', 'postSass'], done);
 });
 
-gulp.task('postcss', ['sass'], function () {
-    let folders = (assets_folder.length) ? [aliasMap[f]] : Object.values(aliasMap);
-    for (folder of folders) {
-        postcssChain(folder);
-    }
-    return 'ok';
+gulp.task('sassOnly', (done) => {
+    return taskRunner(['sass'], done);
 });
 
-gulp.task('default', ['postcss']);
-gulp.task('main', ['postcss']);
+gulp.task('postSassOnly', (done) => {
+    return taskRunner(['postSass'], done);
+});
+
+gulp.task('doc', (done) => {
+    return taskRunner(['doc'], done);
+});
+
+gulp.task('default', gulp.series('css'));
+gulp.task('main', gulp.series('css'));
---
+++ tasks/browserslist-update.js
@@ -0,0 +1,9 @@
+//const lite = require('caniuse-lite');
+//const altSA = require('caniuse-lite/data/regions/alt-sa.js');
+//const writeJsonFile = require('write-json-file');
+//
+//writeJsonFile('browserslist-alt-SA.json', lite.region(altSA)).then(() => {
+//    console.log('done');
+//});
+
+console.log('refreshed caniuse db, to update custom usage data see https://github.com/ai/browserslist#custom-usage-data\n');
